d1568e01ff7693288ebe75354206e6c14d49f661
ismail hassan <ismailshariff350@gmail.com>
2024-11-29 09:00:12 -0600

    tested my function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,12 +7,33 @@ function addItem(item) {
     basket.push(item);
     return true;
 }
+//Testing function
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+console.log('Adding bananas (expect true)', addItem('bananas'));
+console.log(`Basket is now ${basket}`);
+
+
+
  function listItems () {
     for (let i = 0; i< basket.length; i++) {
         console.log(basket[i]);
     }
 
+ } 
+
+ function empty () {
+    basket.length = 0;
  }
+  console.log('Listing items:');
+ listItems();
+ console.log('Emptying basket (expect true)', empty());
+ console.log(`Basket is now ${basket}`);
+
+
+
+
 
 
 
